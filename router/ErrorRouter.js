a657651264549e1ac38197092ddf9476a8b1c41a
DaeHyun2121 <wheogus9611@naver.com>
2021-03-08 08:39:46 +0900

    router 경로 수정

--- router/ErrorRouter.js
+++ router/ErrorRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/error', function (req, res) {
+app.get('/', function (req, res) {
     res.render('error');
 });
 
--- router/MainRouter.js
+++ router/MainRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/main', function (req, res) {
+app.get('/', function (req, res) {
     res.render('main');
 });
 
