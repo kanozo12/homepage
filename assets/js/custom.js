8c87c5029fbad820077ca6d4ad2cd80eb2a1d98e
DaeHyun2121 <wheogus9611@naver.com>
2021-03-09 08:51:57 +0900

    파일명 변경

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,18 +7,3 @@ $(function() {
         }
     });
 });
-
-// google map
-function initMap() {
-    var seoul = { lat: 37.538326 ,lng: 127.077379 };
-    map = new google.maps.Map( document.getElementById('map'), {
-        zoom: 16,
-        center: seoul
-      });
-  
-    new google.maps.Marker({
-      position: seoul,
-      map: map,
-      label: "솔모 정보 기술"
-    });
-  }
