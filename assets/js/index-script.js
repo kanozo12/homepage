1ca2e601536aac5701b4931fd53bbb5776c7c491
YooJunWoo <41713160+kanozo12@users.noreply.github.com>
2021-03-09 16:31:43 +0900

    main page publish

---
+++ assets/js/index-script.js
@@ -0,0 +1,13 @@
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 129) {
+            $(".move-nav").addClass("active");
+            $(".icon-container img:nth-child(1)").attr("src", "../../assets/img/index/white_sitemap.png");
+            $(".icon-container img:nth-child(2)").attr("src", "../../assets/img/index/white_setup-dots.png");
+        } else {
+           $(".move-nav").removeClass("active");
+           $(".icon-container img:nth-child(1)").attr("src", "../../assets/img/index/sitemap.png");
+           $(".icon-container img:nth-child(2)").attr("src", "../../assets/img/index/setup-dots.png");
+        }
+    });
+});
