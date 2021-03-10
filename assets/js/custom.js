2badc41afab3843e42fbdab3a6f891573f075406
YooJunWoo <41713160+kanozo12@users.noreply.github.com>
2021-03-11 08:44:41 +0900

    modify

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 $(function() {
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 129) {
+        if($(window).scrollTop() > 100) {
             $(".move-nav").addClass("active");
             $(".icon-container img:nth-child(1)").attr("src", "../../assets/img/index/white_sitemap.png");
             $(".icon-container img:nth-child(2)").attr("src", "../../assets/img/index/white_setup-dots.png");
