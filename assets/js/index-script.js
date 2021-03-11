095bbb4b9c29ebc03c7c0131ae642ed535d60570
DaeHyun2121 <wheogus9611@naver.com>
2021-03-11 16:41:17 +0900

    dev

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -2,12 +2,12 @@ $(function() {
     $(window).on("scroll", function() {
         if($(window).scrollTop() > 100) {
             $(".move-nav").addClass("active");
-            $(".icon-container img:nth-child(1)").attr("src", "../../assets/img/index/white_sitemap.png");
-            $(".icon-container img:nth-child(2)").attr("src", "../../assets/img/index/white_setup-dots.png");
+            $(".icon-container .tag:nth-child(1) img").attr("src", "../../assets/img/index/white_sitemap.png");
+            $(".icon-container .tag:nth-child(2) img").attr("src", "../../assets/img/index/white_setup-dots.png");
         } else {
            $(".move-nav").removeClass("active");
-           $(".icon-container img:nth-child(1)").attr("src", "../../assets/img/index/sitemap.png");
-           $(".icon-container img:nth-child(2)").attr("src", "../../assets/img/index/setup-dots.png");
+           $(".icon-container .tag:nth-child(1) img").attr("src", "../../assets/img/index/sitemap.png");
+           $(".icon-container .tag:nth-child(2) img").attr("src", "../../assets/img/index/setup-dots.png");
         }
     });
 });
